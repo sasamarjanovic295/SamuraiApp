53c4509f07023d47226d314210348a4d212cdc49
Sasa Marjanovic <sasa.marjanovic295@gmail.com>
2022-07-19 14:02:20 +0200

    Initial commit

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+	public class SamuraiContext : DbContext
+	{
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=192.168.0.112, 1434;Initial Catalog=SamuraiAppDb; User ID=novistudent;Password=novistudent");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
+
---
+++ SamuraiApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using SamuraiApp.Data;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.UI
+{
+    class Program
+    {
+        private static SamuraiContext _context = new SamuraiContext();
+        public static void Main()
+        {
+            _context.Database.EnsureCreated();
+            GetSamurais("Before Add:");
+            AddSamurai("saleZmaj");
+            GetSamurais("After Add:");
+            System.Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+        static void AddSamurai(string name)
+        {
+            var samurai = new Samurai { Name = name};
+            _context.Add(samurai);
+            _context.SaveChanges();
+        }
+        static void GetSamurais(string text)
+        {
+            var samurais = _context.Samurais.ToList();
+            System.Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach (var samurai in samurais)
+            {
+                System.Console.WriteLine(samurai.Name);
+            }
+        }
+    }
+}
